c80862f5ff9b441ae4c74b912d3c2987c465b574
Edward Owens <edwowe99@gmail.com>
2025-08-04 22:40:30 +0100

    updated job history

--- src/config.ts
+++ src/config.ts
@@ -4,16 +4,44 @@ export const siteConfig = {
     description: "Portfolio website of Edward Owens",
     accentColor: "#1d4ed8",
     social: {
-        email: "your-email@example.com",
-        linkedin: "https://linkedin.com/in/yourprofile",
-        twitter: "https://x.com/rfitzio",
-        github: "https://github.com/RyanFitzgerald",
+        email: "edwowe99@gmail.com",
+        linkedin: "https://www.linkedin.com/in/edward-p-owens/",
+        github: "https://github.com/edwowe99",
     },
     aboutMe:
         "I'm playing around with astro to make a personal github page. If you've stumbled across this, I hope you have a good day :)",
     skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
     projects: [],
-    experience: [],
+    experience: [
+        {
+            company: "Department for Education",
+            title: "Principal Data Scientist",
+            dateRange: "Nov 2024 - Present",
+            bullets: [
+                "Led development of AI consultation and survey analysis, saving ~£50k per month on contracted thematic analysis.",
+                "Leading on agentic AI enquiries automation, targetting FTE saving by automating call summarisation, CRM ticket triaging and response drafting.",
+                "Line managing team of 2x Senior Data Scientists and 2x Data Scientists",
+                ],
+            },
+            {
+            company: "Department for Education",
+            title: "Senior Data Scientist - Children's Social Care",
+            dateRange: "July 2023 - Nov 2024",
+            bullets: [
+                "Full stack development into production of an innovative 'Children\'s Social Care AI', an agentic LLM assistant that could answer natural language statistics questions using RAG and function calling.",
+                "Distribution of a multi-billion pound funding settlement to Local Authorities for Children and Young Person's services, using a child-level, needs based, hierarchical regression model.",
+            ],
+            },
+            {
+            company: "Valuation Office Agency",
+            title: "Senior Analyst",
+            dateRange: "September 2022 - July 2023",
+            bullets: [
+                "Improved predictive power of an Automated Valuation Model model used to value properties in Wales for Council Tax bandings by Leading on “comparables” workstream",
+                "Transformed a key function of the business by delivering Comparable Selection Tool, a web app that centralised data and streamlined the valuation process",
+            ],
+            },
+    ],
     education: [
         {
         school: "University of Bath",
