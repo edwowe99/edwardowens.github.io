ec3d8ef2dc491a239d0106c414c2745883821603
OWENS <Edward.OWENS@EDUCATION.GOV.UK>
2025-08-04 16:35:37 +0100

    updated file strucutre

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    name: "Edward Owens",
+    title: "Principal AI Data Scientist & Full Stack developer",
+    description: "Portfolio website of Edward Owens",
+    accentColor: "#1d4ed8",
+    social: {
+        email: "your-email@example.com",
+        linkedin: "https://linkedin.com/in/yourprofile",
+        twitter: "https://x.com/rfitzio",
+        github: "https://github.com/RyanFitzgerald",
+    },
+    aboutMe:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
+    
+}
